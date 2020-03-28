53e378b0070b56737a64ddf7b3635350a2c138e4
Calo Corp <marc.calo@codeimmersives.com>
2020-03-28 19:41:33 -0400

    stuck on looping through object and pushing into array

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
-const getTodoName = function() {}
-const getPriority = function() {}
-const isComplete = function() {}
-const isHighPriority = function() {}
+const getTodoName = function(todos) {
+  return todos.text 
+}
+
+const getPriority = function(todos) {
+  return todos.priority
+}
+const isComplete = function(todos) {
+
+  return (todos.complete === true)
+}
+const isHighPriority = function(todos) {
+
+  return (todos.priority === 2)
+}
 
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
-const names = function() {}
+const names = function(todos) {
+  const todoNames=[]
+getTodoName.push.(todoNames)
+}
 const priorities = function() {}
 const namesAndPriorities = function() {}
 const justNotComplete = function() {}
