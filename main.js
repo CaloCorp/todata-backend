6f066466e8bebf4aa9280e695af12d0778bfbdaf
Calo Corp <marc.calo@codeimmersives.com>
2020-03-30 07:39:56 -0400

    all but one

--- main.js
+++ main.js
@@ -12,24 +12,51 @@ const isComplete = function(todos) {
 
   return (todos.complete === true)
 }
+
+const isNotComplete = function (todos) {
+
+  return (todos.complete === false)
+}
 const isHighPriority = function(todos) {
 
   return (todos.priority === 2)
 }
 
+const isLowPriority = function (todos) {
+
+  return (todos.priority === 1)
+}
+
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
-const names = function(todos) {
-  const todoNames=[]
-getTodoName.push.(todoNames)
+const names = function(todos) { 
+  return todos.map(getTodoName)
 }
-const priorities = function() {}
-const namesAndPriorities = function() {}
-const justNotComplete = function() {}
-const justComplete = function() {}
-const priority2Only = function() {}
-const priority1Only = function() {}
+const priorities = function(todos) {
+
+  return todos.map(getPriority)
+}
+const namesAndPriorities = function(todos) {
+
+  return todos.map(getPriority)
+}
+const justNotComplete = function(todos) {
+
+  return todos.filter(isNotComplete)
+}  
+const justComplete = function(todos) {
+
+  return todos.filter(isComplete)
+}
+const priority2Only = function(todos) {
+
+  return todos.filter(isHighPriority)
+}
+const priority1Only = function(todos) {
+
+ return todos.filter(isLowPriority)
+}
 
 
 module.exports = {
